43adc264ac596c40df508c8d68ec1f8744b79a41
bakadachi <jani.silvander@saunalahti.fi>
2024-03-07 14:52:36 +0200

    ck2ck signals and regs

--- sim/tb/asTest_Ck2CkFifo.sv
+++ sim/tb/asTest_Ck2CkFifo.sv
@@ -1,22 +1,23 @@
 module asTest_Ck2CkFifo # (
   DATA_W = 8,
-  FIFO_W = 16
+  FIFO_SIZE   = 4
+  FIFO_ADDR_W = clog2(FIFO_SIZE),
 ) (
-  input logic                           ckSlow,  
-  input logic                           ckFast,  
-  input logic                           arstSlow,
-  input logic                           arstFast,
-  input logic [DATA_W-1:0]              dataIn,
-  input logic                           pop,
-  input logic                           empty,
-  input logic [DATA_W-1:0]              push,
-  input logic                           full,
-  input logic [FIFO_W-1:0]              pointer,
-  input logic [FIFO_W-1:0][DATA_W-1:0]  fifo_rg,
+  input logic                                ckSlow,  
+  input logic                                ckFast,
+  input logic                                arstSlow,
+  input logic                                arstFast,
+  input logic [DATA_W-1:0]                   dataIn,
+  input logic                                pop,
+  input logic                                empty,
+  input logic [DATA_W-1:0]                   push,
+  input logic                                full,
+  input logic [FIFO_ADDR_W-1:0][DATA_W-1:0]  fifo_rg,
 );
 
-  logic [FIFO_W-1:0]              pointer,
-  logic [FIFO_W-1:0][DATA_W-1:0]  fifo_rg,
+  logic [FIFO_SIZE-1:0]                firstIn,
+  logic [FIFO_SIZE-1:0]                pointer,
+  logic [FIFO_ADDR_W-1:0][DATA_W-1:0]  fifo_rg,
 
   ty_Ck2CkFifoStates currStCk2CkFifo;
   ty_Ck2CkFifoStates nextStCk2CkFifo;
