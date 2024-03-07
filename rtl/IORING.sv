b3674f3b5698949207054772a4de3e9ee40e1a3e
bakadachi <jani.silvander@saunalahti.fi>
2024-03-07 08:27:25 +0200

    IORING FLASH, SDRAM

--- rtl/IORING.sv
+++ rtl/IORING.sv
@@ -24,20 +24,43 @@ module IORING # (
   output logic [7:0]          HEX1,                         // B15 , A15 , E14 , B14 , A14 , C13 , B13 , A13
   output logic [7:0]          HEX2,                         // A18 , F14 , B17 , A17 , E15 , B16 , A16 , D15
   output logic [7:0]          HEX3,                         // G16 , G15 , D19 , C19 , B19 , A19 , F15 , B18
-  output logic [3:0]          VGAR,                         // R3  , R2  , R1  , R0
-  output logic [3:0]          VGAG,                         // G3  , G2  , G1  , G0
-  output logic [3:0]          VGAB,                         // G3  , G2  , G1  , G0
+  output logic [3:0]          VGAR,                         // H21 , H20 , H17 , H19
+  output logic [3:0]          VGAG,                         // J21 , K17 , J17 , H22
+  output logic [3:0]          VGAB,                         // K18 , J22 , K21 , K22
+  output logic                VGAH                          // L21
   output logic                VGAV,                         // L22
-  output logic                VGAH                          // L22
-  output logic                RXD                           // U22 [ADM3202]
-  output logic                RTS                           // V22 [ADM3202]
+  output logic                RXD,                          // U22 [ADM3202]
+  output logic                RTS,                          // V22 [ADM3202]
   // Bidirectional
-  inout logic                 SDCMD                         // Y22
-  inout logic                 SDDAT0                        // AA22
-  inout logic                 SDDAT3                        // W21
+  inout logic                 SDCMD,                        // Y22
+  inout logic                 SDDAT0,                       // AA22
+  inout logic                 SDDAT3,                       // W21
   inout logic [31:0]          GPIO0,                        // tl;dw 
   inout logic [31:0]          GPIO1,                        // tl;dw 
-
+  // SDRAM_IF
+  output logic [12:0]         DRAM_ADDR,
+  inout  logic [15:0]         DRAM_DQ,
+  output logic                DRAM_BA_0,
+  output logic                DRAM_BA_1,
+  output logic                DRAM_LDQM,
+  output logic                DRAM_UDQM,
+  output logic                DRAM_WE_N,
+  output logic                DRAM_CAS_N,
+  output logic                DRAM_RAS_N,
+  output logic                DRAM_CAS_N,
+  output logic                DRAM_CLK,
+  output logic                DRAM_CKE,
+  // FLASH_IF
+  output logic [12:0]         FL_ADDR,
+  inout  logic [15:0]         FL_DQ,
+  output logic                FL_DQ_AM1,
+  output logic                FL_WE_N,
+  output logic                FL_RST_N,
+  output logic                FL_WP_N,
+  output logic                FL_RY,
+  output logic                FL_CE_N,
+  output logic                FL_OE_N,
+  output logic                FL_BYTE_N,
 );
 
 logic SdCmdIn;
@@ -55,6 +78,9 @@ logic [31:0] Gpio0En;
 logic [31:0] Gpio1In;
 logic [31:0] Gpio1Out;
 logic [31:0] Gpio1En;
+logic [15:0] DramDatIn;
+logic [15:0] DramDatOut;
+logic        DramDatEn;
 
 // inout assignment for SD_CMD
 assign SDCMD    = SdCmdEn ? SdCmdOut  : 'z;
@@ -68,13 +94,17 @@ assign SdDat0In = SDDAT0;
 assign SDDAT3   = SdCmdEn  ? SdDat3Out : 'z;
 assign SdDat3In = SDDAT3;
 
+// inout assignment for SDRAM
+assign DRAM_DQ = DramDatEn ? DramDatOut : 'z;
+assign DramDatIn = DRAM_DQ;
+
 // inout assignments for GPIO
 genvar i;
 generate
-for (i=0 i<32; i=i+1) {
+for (i=0 i<31; i=i+1) begin
   assign GPIO0 = Gpio0En[i] ? Gpio0Out[i] : 'z;
   assign GPIO1 = Gpio1En[i] ? Gpio1Out[i] : 'z;
-} 
+end
 endgenerate
 
 
